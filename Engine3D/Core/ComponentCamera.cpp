6538a093621eef9dceaec87cc72759198be06c25
MaxLlovera <61042419+MaxLlovera@users.noreply.github.com>
2021-12-02 13:03:56 +0100

    FOV started
    Field of view: the math

---
+++ Engine3D/Core/ComponentCamera.cpp
@@ -0,0 +1,56 @@
+#include "ComponentCamera.h"
+#include "Application.h"
+
+
+//ComponentCamera::ComponentCamera()
+//{
+//
+//}
+
+ComponentCamera::ComponentCamera(GameObject* parent) : Component(parent)
+{
+
+}
+
+ComponentCamera::~ComponentCamera()
+{
+
+}
+
+void ComponentCamera::NearPlane(float dist)
+{
+	frustrum.nearPlaneDistance = dist;
+}
+
+void ComponentCamera::FarPlane(float dist)
+{
+	frustrum.farPlaneDistance = dist;
+}
+
+void ComponentCamera::VerticalFOV(float vFOV, float width, float height)
+{
+	frustrum.verticalFov = vFOV;
+	frustrum.horizontalFov = 2.f * atan((tan(frustrum.verticalFov * 0.5)) * (width/height));
+}
+
+void ComponentCamera::HorizontalFOV(float hFOV, float width, float height)
+{
+	frustrum.horizontalFov = hFOV;
+	frustrum.verticalFov = 2.f * atan((tan(frustrum.horizontalFov * 0.5)) * (width / height));
+}
+
+float* ComponentCamera::GetProjectionMat()
+{
+	float4x4 projMat;
+	projMat = frustrum.ProjectionMatrix();
+
+	return (float*) projMat.v;
+}
+
+float* ComponentCamera::GetViewMat()
+{
+	float4x4 viewMat;
+	frustrum.ViewMatrix();
+
+	return (float*)viewMat.v;
+}
--- Engine3D/Core/ComponentCamera.h
+++ Engine3D/Core/ComponentCamera.h
@@ -3,25 +3,34 @@
 // -- Tools
 #include <vector>
 #include <string>
-#include "Core/Globals.h"
-#include "Core/Component.h"
-
+#include "Globals.h"
+#include "Component.h"
+#include "MathGeoLib/include/MathGeoLib.h"
 #include "Geometry/Frustum.h"
 
 class GameObject;
 
-class ComponentCamera: public Component {
+class ComponentCamera : public Component{
 
 public:
 
-	ComponentCamera();
-	ComponentCamera(GameObject* gameObject);
+	//ComponentCamera();
+	ComponentCamera(GameObject* parent);
 	~ComponentCamera();
 
 	void Update();
 	void OnSave();
 	void OnLoad();
 
-public:
+	void NearPlane(float dist);
+	void FarPlane(float dist);
+	void VerticalFOV(float vFOV, float width, float height);
+	void HorizontalFOV(float hFOV, float width, float height);
 
+	float* GetProjectionMat();
+	float* GetViewMat();
+
+
+public:
+	Frustum frustrum;
 };
